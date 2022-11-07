9f467577c54a388b76d3d22ea395e1b4976a6669
zerotower69 <zerotower@163.com>
2022-11-07 08:51:49 +0800

    feature:环境搭建，基本配置验证完成

---
+++ web-plus/src/main-prod.ts
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+//scrollto from vue3 please see https://github.com/rigor789/vue-scrollto/issues/380
+import VueScrollTo from 'vue-scrollto'
+//element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//mavon-editor for vue3 please see 
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import '@/assets/style/common.css'
+import '@/assets/font/iconfont'
+// 网站统计
+/* eslint-disable */
+//@ts-ignore
+var _hmt = _hmt || []
+//@ts-ignore
+window._hmt = _hmt; // 将_hmt挂载到window下
+(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?a1c0040f374f384b2d4a876ecdc1cf5d'
+  var s:HTMLElement = document.getElementsByTagName('script')[0]
+  s.parentNode?.insertBefore(hm, s)
+})()
+/* eslint-enable */
+const app = createApp(App);
+app.directive('scroll-to', VueScrollTo);
+
+app.use(ElementPlus).use(mavonEditor).use(store).use(router).mount('#app')
---
+++ web-plus/src/request.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import type { AxiosRequestConfig} from 'axios'
+const instance = axios.create({
+  // baseURL: '/api/v1', // 开发模式下
+  // baseURL: 'https://xanadu.aerowang.cn/api/v1', // 线上模式
+  baseURL: process.env.VUE_APP_BASEURL,
+  // baseURL: 'http://123.56.124.33:3000/api/',
+  // baseURL: 'https://www.fastmock.site/mock/c189d5ce3b7cb372ce6f287aa8bc6238/api',
+  // 请求超时时间
+  timeout: 10000
+})
+
+instance.interceptors.request.use((config:AxiosRequestConfig) => {
+  // console.log(config)
+    config.headers?.common?.set('Authorization',window.sessionStorage.getItem('token'))
+//   config?.headers?.Authorization = 'Bearer ' + window.sessionStorage.getItem('token')
+  return config
+})
+
+export default instance
